0e04e3b8c3655a24c0a71c705d2d444780e60e95
Kellie Manasco <kellie.manasco@gmail.com>
2021-04-15 15:15:49 -0400

    nav bar now generates though JS instead of being written in HTML file. Underline now appears under the current tab being viewed.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import loadHome from './home';
 import loadMenu from './menu';
 import loadContact from './contact';
 
+function loadNav() {
+    const body = document.querySelector('.body');
+    const nav = document.createElement('nav');
+    nav.classList.add('navbar');
+    nav.innerHTML = '<div class="tabs underline" id="home">Home</div><div class="tabs" id="menu">Menu</div><div class="tabs" id="contact">Contact</div>';
+    body.insertBefore(nav, body.childNodes[0]);
+}
+
+document.addEventListener('DOMContentLoaded', loadNav());
+
+document.addEventListener('DOMContentLoaded', loadHome);
+
 const homeTab = document.querySelector('#home');
 const menuTab = document.querySelector('#menu');
 const contactTab = document.querySelector('#contact');
@@ -9,3 +21,19 @@ const contactTab = document.querySelector('#contact');
 homeTab.addEventListener('click', loadHome);
 menuTab.addEventListener('click', loadMenu);
 contactTab.addEventListener('click', loadContact);
+
+homeTab.addEventListener('click', function(){
+    this.classList.add('underline');
+    menuTab.classList.remove('underline');
+    contactTab.classList.remove('underline');
+});
+menuTab.addEventListener('click', function(){
+    this.classList.add('underline');
+    homeTab.classList.remove('underline');
+    contactTab.classList.remove('underline');
+});
+contactTab.addEventListener('click', function(){
+    this.classList.add('underline');
+    homeTab.classList.remove('underline');
+    menuTab.classList.remove('underline');
+});
